b2c6bd5e05ba3977eaa1f8919415fcf7c6527688
Suntle <mogintent@gmail.com>
2024-02-14 17:14:39 -0500

    euybf

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,15 @@ class $modify(EndLevelLayer){
 	void customSetup() {
 		EndLevelLayer::customSetup();
 		// add yo ui stuff here
+		auto winSize = CCDirector::get()->getWinSize();
 		auto WinLayer = static_cast<cocos2d::CCLayer*>(this->getChildren()->objectAtIndex(0));
 		auto label = CCLabelBMFont::create("Hello End Screen", "bigFont.fnt");
         label->setPosition(100, 100);
         WinLayer->addChild(label);
+
+		auto SS = CCLabelBMFont::create("_", "bigFont.fnt");
+        SS->setPosition(winSize.width, winSize.height);
+		SS->setID("Window Helper");
+        WinLayer->addChild(SS);
 	}
 };
