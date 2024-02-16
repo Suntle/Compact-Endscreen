86600c8f341f0f1dc977738bc4bb9d533499f173
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-02-16 16:08:43 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -137,7 +137,7 @@ for(auto child : CCArrayExt<CCNode*>(WinLayer->getChildren())) {
         WinLayer->addChild(SS);
 		*/
 		
-		
+		DONOTCRASH = false
 		WinLayer->getChildByID("Level-Complete")->setVisible(false);
 		WinLayer->getChildByID("Chain_Right")->setVisible(false);
 		WinLayer->getChildByID("Chain_Left")->setVisible(false);
@@ -157,12 +157,11 @@ for(auto child : CCArrayExt<CCNode*>(WinLayer->getChildren())) {
 		WinLayer->getChildByID("Attempts-label")->setPosition(73,winSize.height -125);
 		WinLayer->getChildByID("Jump-label")->setPosition(73,winSize.height -149);
 		WinLayer->getChildByID("Time-label")->setPosition(73,winSize.height -173);
-		if (DONOTCRASH) {
-			return true
+		if (!DONOTCRASH) {
+			Buttons->getChildByID("Retry")->setPosition(winSize.width-328,winSize.height-206);
+			Buttons->getChildByID("MenuButton")->setPosition(winSize.width-328,-133);
+			Buttons->getChildByID("EditButton")->setPosition(winSize.width-328,-1);	
 		}
-		Buttons->getChildByID("Retry")->setPosition(winSize.width-328,winSize.height-206);
-		Buttons->getChildByID("MenuButton")->setPosition(winSize.width-328,-133);
-		Buttons->getChildByID("EditButton")->setPosition(winSize.width-328,-1);
 	
 
 		
