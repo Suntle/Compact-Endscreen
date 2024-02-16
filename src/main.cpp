78f6552f4bca4191bca89f7c422fe6a2f5f6ed17
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-02-16 16:34:31 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -154,11 +154,11 @@ for(auto child : CCArrayExt<CCNode*>(WinLayer->getChildren())) {
 			WinLayer->getChildByID("LevelVerified_TextField")->setScale(0.5);
 			WinLayer->getChildByID("Attempts-label")->setPosition(73,winSize.height -125);
 			WinLayer->getChildByID("Jump-label")->setPosition(73,winSize.height -149);
+			WinLayer->getChildByID("Background_Textures")->setPosition(-213,32);
 		if (!DONOTCRASH) {
 			WinLayer->getChildByID("Level-Complete")->setVisible(false);
 			WinLayer->getChildByID("Chain_Right")->setVisible(false);
 			WinLayer->getChildByID("Chain_Left")->setVisible(false);
-			WinLayer->getChildByID("Background_Textures")->setPosition(-213,32);
 			WinLayer->getChildByID("Time-label")->setPosition(73,winSize.height -173);
 			Buttons->getChildByID("Retry")->setPosition(winSize.width-328,winSize.height-206);
 			Buttons->getChildByID("MenuButton")->setPosition(winSize.width-328,-133);
