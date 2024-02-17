1d6a65ddda23f921331a7f81c45e50e81584eca0
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-02-17 18:40:39 +0000

    Std::string

--- src/main.cpp
+++ src/main.cpp
@@ -206,6 +206,11 @@ void showLayer(bool p0) {
             }
         }
     }
+	TodoReturn coinEnterFinishedO(cocos2d::CCObject p0) {
+		p0->setScale(0.5);
+		p0->setPosition(167,190);
+		log::info(std::string child->getPosition());
+	}
 	void customSetup() {
 		EndLevelLayer::customSetup();
 		auto WinLayer = static_cast<cocos2d::CCLayer*>(this->getChildren()->objectAtIndex(0)); // CCLAYER REAL
