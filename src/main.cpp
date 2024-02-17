810cb1775caf553f7c9f90d3ae9aa839b334324e
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-02-17 18:43:25 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -206,7 +206,7 @@ void showLayer(bool p0) {
             }
         }
     }
-	TodoReturn coinEnterFinishedO(cocos2d::CCObject p0) {
+	TodoReturn coinEnterFinishedO(cocos2d::CCObject* p0) {
 		p0->setScale(0.5);
 		p0->setPosition(167,190);
 		log::info(std::string child->getPosition());
